5200f5e4ed032ab6254deb7e91d3bacfdb8fa18a
Syaifullah <syaifullah.b.pallu@gmail.com>
2024-06-25 10:24:01 +0800

    update

--- app.py
+++ app.py
@@ -6,9 +6,19 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-@app.route("/")
-def indext():
-    return render_template("base.html")
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.column(db.String(100))
+    complete = db.column(db.Boolean)
+
+@app.route('/')
+def index():
+    todo_list = Todo.query.all()
+    return render_template("base.html") #, todo_list=todo_list)
 
 if __name__ == "__main__":
+#   db.create.all()
+#   new_todo = Todo(title="todo 1", complete=False)
+#   db.session.add(new_todo)
+#   db.session.commit()
     app.run(debug=True)
